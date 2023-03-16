342a5f8f79fda7f17077f06ee6cdf653f462a111
Helena Gingembre <e.izotovagingembre@outlook.com>
2023-03-16 17:59:41 +0100

    style cards User

--- src/components/UserCard/UserCard.jsx
+++ src/components/UserCard/UserCard.jsx
@@ -1,13 +1,42 @@
-import { BoxCard, BoxItem } from './UserCard.style';
+import {
+    Avatar,
+    BoxCard,
+    BoxItem,
+    CardInfo,
+    CardTop,
+    Linear,
+    ImgUser,
+    LoqoIcon,
+    TweetsStats,
+} from './UserCard.style';
+
+import logo from '../../assets/images/Logo.png';
 
 export const CardUser = ({ user }) => {
     const { id, user_name, tweets, followers, avatar } = user;
+
+    console.log('user____>', user);
     return (
         <BoxItem key={id}>
             <BoxCard>
-                <p> {user_name}</p>
-                <p> {tweets}</p>
-                <p> {followers}</p>
+                <CardTop>
+                    <LoqoIcon>
+                        {/* <svg class="icon" width="76" height="22">
+                            <use href={logo}></use>
+                        </svg> */}
+                        <img src={logo} alt="GoIT" />
+                    </LoqoIcon>
+                </CardTop>
+                <Linear>
+                    <Avatar>
+                        <ImgUser src={avatar} alt={user_name} />
+                    </Avatar>
+                </Linear>
+                <CardInfo>
+                    <TweetsStats> {tweets} Tweets</TweetsStats>
+                    <TweetsStats> {followers} Followers</TweetsStats>
+                    <button>Following</button>
+                </CardInfo>
             </BoxCard>
         </BoxItem>
     );
--- src/components/UserCard/UserCard.style.js
+++ src/components/UserCard/UserCard.style.js
@@ -23,9 +23,11 @@ export const BoxItem = styled.li`
 
 export const BoxCard = styled.div`
     display: flex;
+    flex-wrap: wrap;
     width: 380px;
     height: 460px;
     justify-content: center;
+
     background: linear-gradient(
         114.99deg,
         #471ca9 -0.99%,
@@ -34,4 +36,65 @@ export const BoxCard = styled.div`
     );
     box-shadow: -2.5777px 6.87386px 20.6216px rgba(0, 0, 0, 0.23);
     border-radius: 20px;
+`;
+
+export const CardTop = styled.div`
+    position: relative;
+    width: 100%;
+    height: 214px;
+    background-image: url('../../assets/images/picture2_1.png');
+`;
+export const LoqoIcon = styled.div`
+    position: absolute;
+    top: 20px;
+    left: 20px;
+    width: 76px;
+    height: 22px;
+    fill: rgba(255, 255, 255, 0.3);
+`;
+
+export const Linear = styled.div`
+    position: relative;
+    width: 100%;
+    height: 8px;
+    background: #ebd8ff;
+    box-shadow: 0px 3.43693px 3.43693px rgba(0, 0, 0, 0.06),
+        inset 0px -1.71846px 3.43693px #ae7be3,
+        inset 0px 3.43693px 2.5777px #fbf8ff;
+`;
+export const Avatar = styled.div`
+    position: absolute;
+    top: -36px;
+    left: 150px;
+    width: 80px;
+    height: 80px;
+    padding: 8px;
+    border-radius: 50%;
+    background-image: url('../../assets/images/ellipseStroke.png');
+`;
+
+export const ImgUser = styled.img`
+    width: 62px;
+    height: 62px;
+    border-radius: 50%;
+`;
+
+export const CardInfo = styled.div`
+    width: 100%;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding-top: 62px;
+    padding-bottom: 36px;
+`;
+export const TweetsStats = styled.p`
+    margin: 0px auto 16px auto;
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 20px;
+    line-height: 1.2;
+    color: #ebd8ff;
 `;
